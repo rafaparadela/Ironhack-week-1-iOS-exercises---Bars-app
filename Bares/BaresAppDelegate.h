25d362910cce25f702574287f920ec9d25eb7bed
Gorka Magaña <gorka@gorkamm.com>
2014-06-06 09:34:32 +0200

    Initial Commit

---
+++ Bares/BaresAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BaresAppDelegate.h
+//  Bares
+//
+//  Created by Gorka Magaña on 06/06/14.
+//  Copyright (c) 2014 Ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaresAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Bares/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Bares
+//
+//  Created by Gorka Magaña on 06/06/14.
+//  Copyright (c) 2014 Ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BaresAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BaresAppDelegate class]));
+    }
+}
